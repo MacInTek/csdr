d06a0e094777c70e0299d190959833db6977db90
Mac Ojie Riveral <94472582+MacInTek@users.noreply.github.com>
2026-05-19 21:34:24 +0800

    Remove sensitive data from firebase config
    Removed sensitive Firebase configuration details.

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -8,14 +8,14 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-s
 
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBtC1en8dJodBek7MgXEYZwtq0Ci5CJ0Jg",
-  authDomain: "csltiv.firebaseapp.com",
-  databaseURL: "https://csltiv-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "csltiv",
-  storageBucket: "csltiv.firebasestorage.app",
-  messagingSenderId: "742195537367",
-  appId: "1:742195537367:web:ae19b00b244e47ebb99792",
-  measurementId: "G-YGJRZSCVCH"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
